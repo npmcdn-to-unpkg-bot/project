f769a2f85eee5e766fbaf13d20d1817874036ecb
fanchaoyang <yangyang9066@hotmail.com>
2016-06-16 18:19:10 +0800

    1014

---
+++ 1014/src/index.js
@@ -0,0 +1,101 @@
+
+var font = (function(){
+	var defaults={
+		length : 36,
+		colorList : [],
+		transformList : [],
+		element : {}
+	},game={},options={},
+	  bout=0,index,time=10,timer,state=false,
+	  text = ['祝','父','亲','节','快','乐'],
+	  tempList=[];
+	game.init=function(){
+		// for(var i=1 ;i<options.length;i++){
+		// 	options.font.push(game.font());
+		// }
+		tempList = options.font.concat(text[bout]);
+		tempList = shuffle(tempList);
+		index = tempList.indexOf(text[bout]);
+		// console.log(tempList)
+		// console.log(index)
+		var str = '';
+		tempList.forEach(function(e){
+			str+='<span class="'
+				 +options.colorList[Math.floor(Math.random()*options.colorList.length)]
+				 +' '
+				 +options.transformList[Math.floor(Math.random()*options.transformList.length)]
+				 +'">'+e+'</span>';
+		})
+		options.element.find('.content').html(str);
+		options.element.find('.title span').eq(bout).addClass('on').siblings().removeClass('on');
+		for(var j=0 ;j<bout;j++){
+			options.element.find('.title span').eq(j).addClass('active');
+		}
+		time = 10;
+		game.play();
+	}
+	game.play = function(){
+		function clickFont(){
+			// console.log($(this).index())
+			if($(this).index() == index){
+				clearInterval(timer);
+				bout++;
+				console.log(bout)
+				console.log(text.length)
+				if(bout>=text.length){
+					alert('送你一个吻！！！')
+					return;
+				}
+				game.init();
+			}
+		}
+		function timerFun(){
+			time--;
+			$('.time span').html(time);
+			if(time <= 0){
+				state = true;
+				clearInterval(timer);
+				options.element.find('.content').find('span').each(function(index, el) {
+					this.removeEventListener('touchstart',clickFont)
+				});
+			}
+		}
+
+		// 监听文字点击
+		options.element.find('.content').find('span').each(function(index, el) {
+			this.addEventListener('touchstart',clickFont)
+		});
+
+		$('.time span').html(time);
+		//监听
+		timer = setInterval(timerFun,1000);
+	}
+	game.font=function(){
+	    eval( "var word=" +  '"\\u' + (Math.round(Math.random() * 20901) + 19968).toString(16)+'"');
+	    return word;
+	}
+	return {
+		init:function(opt){
+			options = $.extend(defaults,opt);
+			options.font = ['站','无','设','你','建','设',
+							'冰','享','附','近','家','加',
+							'的','自','房','间','啊','热',
+							'及','第','吹','爱','云','好',
+							'吃','计','才','陪','我','奇',
+							'迹','去','藕','马','甲'];
+			options.length = 36;
+		    game.init();
+		}
+	}
+})();
+
+
+
+$(function(){
+	font.init({
+		element : $('#game'),
+		colorList : ['c1','c2','c3','c4','c5','c6'],
+		transformList : ['t1','t2','t3','t4']	
+	});
+})
+
--- gulpfile.js
+++ gulpfile.js
@@ -11,19 +11,19 @@ var gulp = require('gulp'),
 
 //sass
 gulp.task('sass',function(){
-  return gulp.src('*/src/*.scss')
+  return gulp.src('1014/src/*.scss')
              .pipe(sass())
              .on('error', function(err) {
                   gutil.log('Less Error!', err.message);
                   this.end();
               })
-             .pipe(gulp.dest('.'));
+             .pipe(gulp.dest('1014/src/'));
 })
 // 文件刷新
 gulp.task('livereload', function() {
   gulp.src(['*/*.html','!1001/*.html','!1002/*.html','!1003/*.html','!1004/*.html'
             ,'!1005/*.html','!1006/*.html','!1007/*.html','!1008/*.html','!1009/*.html'
-            ,'!1010/*.html','!1011/*.html','!1012/*.html','!1tel/*.html','!animated/*.html'
+            ,'!1010/*.html','!1011/*.html','!1012/*.html','!1013/*.html','!1tel/*.html','!animated/*.html'
             ,'!canvas/*.html','!default/*.html','!fish/*.html','!map/*.html'
             ,'!tableToExcel/*.html','!1100/*.html','!_public/*.html'])
       .pipe(livereload());
@@ -70,8 +70,8 @@ gulp.task('rename', function () {
 gulp.task('watch', function() {
   livereload.listen(); //要在这里调用listen()方法
   // gulp.watch(['1*/**','!1*/**/*.min.js','!1*/**/*.min.css','_public/*.css','!_public/*.min.css','_public/*.js','!_public/*.min.js'], ['livereload']);
-  gulp.watch(['1*/**','_public/*.css','_public/*.js'], ['livereload']);
-  // gulp.watch(['1*/**','_public/*.css','_public/*.js'], ['sass','livereload']);
+  // gulp.watch(['1*/**','_public/*.css','_public/*.js'], ['livereload']);
+  gulp.watch(['1*/**','_public/*.css','_public/*.js'], ['sass','livereload']);
 });
 
 gulp.task('canvas', function() {
