cf1e5457eb5cd70c3a6d9fc7422252e086d81546
fanchaoyang <yangyang9066@hotmail.com>
2016-07-18 15:10:17 +0800

    sd

--- 1022/src/load.js
+++ 1022/src/load.js
@@ -6,36 +6,37 @@ function load(list){
 
   var length = img.length + js.length , num = 0;
 
+  function progress(num){
+    var progress = parseInt(num*100/length);
+    console.log(progress)
+    document.getElementsByClassName('loading')[0].getElementsByClassName('tip')[0].innerHTML=progress+"%";
+    if(num == length){
+      document.getElementsByClassName('loading')[0].style.display='none';
+      document.body.className = document.body.className +' ready';
+    }
+  }
   function imgload(src){
     var img = new Image();
     img.onload = function () {
         num++;
-        var progress = parseInt(num*100/length);
-        console.log(progress)
-        document.getElementsByClassName('loading')[0].getElementsByClassName('tip')[0].innerHTML=progress+"%";
-        if(num == length){
-          document.getElementsByClassName('loading')[0].style.display='none';
-          document.body.className = document.body.className +' ready';
-        }
+        progress(num);
     };
     img.src = src;
   }
 
-  function jsload(src){
+  function jsload(list,index){
+    if(index == undefined ) index = 0;
+    if(index >= list.length) return
     var _doc=document.getElementsByTagName('head')[0];
     var script=document.createElement('script');
         script.setAttribute('type','text/javascript');
-        script.setAttribute('src',src);
+        script.setAttribute('src',list[index]);
      _doc.appendChild(script);
      script.onload=function(){
+        index++
+        jsload(list,index);
         num++;
-        var progress = parseInt(num*100/length);
-        console.log(progress)
-        document.getElementsByClassName('loading')[0].getElementsByClassName('tip')[0].innerHTML=progress+"%";
-        if(num == length){
-          document.getElementsByClassName('loading')[0].style.display='none';
-          document.body.className = document.body.className +' ready';
-        }
+        progress(num);
      }
 
   }
@@ -44,8 +45,6 @@ function load(list){
     imgload(img[i]);
   }
 
-  for(var i=0;i<js.length;i++){
-    jsload(js[i]);
-  }
+  jsload(js);
 
 }
