42b3deb6842395fd3b0f43e2f4a088268fe33bb2
fanchaoyang <yangyang9066@hotmail.com>
2016-07-18 18:15:31 +0800

    11

--- 1022/src/load.js
+++ 1022/src/load.js
@@ -2,10 +2,7 @@ function load(list){
   if(list == undefined ) return
   var img = list.img == undefined ? [] : list.img ;
   var js = list.js == undefined ? [] : list.js ;
-
-
   var length = img.length + js.length , num = 0;
-
   function progress(num){
     var progress = parseInt(num*100/length);
     console.log(progress)
@@ -23,7 +20,6 @@ function load(list){
     };
     img.src = src;
   }
-
   function jsload(list,index){
     if(index == undefined ) index = 0;
     if(index >= list.length) return
@@ -38,13 +34,9 @@ function load(list){
         num++;
         progress(num);
      }
-
   }
-
   for(var i=0;i<img.length;i++){
     imgload(img[i]);
   }
-
   jsload(js);
-
 }
