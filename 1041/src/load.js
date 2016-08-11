61af1987110bc30ec0dc416ae83660d36863ab2b
fanchaoyang <yangyang9066@hotmail.com>
2016-08-11 13:39:46 +0800

    12

--- 1041/src/load.js
+++ 1041/src/load.js
@@ -4,7 +4,7 @@ function load(list){
   var js = list.js == undefined ? [] : list.js ;
   var css = list.css == undefined ? [] : list.css ;
   var audio = list.audio == undefined ? [] : list.audio ;
-  var length = img.length + js.length , num = 0;
+  var length = img.length + js.length + audio.length , num = 0;
   function progress(num){
     var progress = parseInt(num*100/length);
     // console.log(progress)
